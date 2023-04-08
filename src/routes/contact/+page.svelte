a045b4ed8ab7e4a26e69a3bfeb8a19df12e028e8
Will Rogers <wahrogers@gmail.com>
2023-04-08 19:56:31 +0100

    Add a form.

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,3 +5,21 @@
 <h1>Contact</h1>
 
 <p>Here is how you can contact us.</p>
+<form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+	<input type="hidden" name="form-name" value="contact" />
+	<p>
+		<label for="yourname"> Your Name: </label> <br />
+		<input type="text" name="name" id="yourname" />
+	</p>
+	<p>
+		<label for="youremail"> Your Email: </label> <br />
+		<input type="email" name="email" id="youremail" />
+	</p>
+	<p>
+		<label for="yourmessage"> Message: </label> <br />
+		<textarea name="message" id="yourmessage" />
+	</p>
+	<p>
+		<button type="submit">Send</button>
+	</p>
+</form>
